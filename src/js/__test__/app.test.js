891c958aee72170c8271aa1409432380994a4377
Deizee <deizee@mail.ru>
2021-02-18 19:11:08 +0500

    add tests

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,49 @@
+import orderByProps from '../app';
+
+test('Возвращает корректное значение', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+
+  expect(orderByProps(obj, ['name', 'level'])).toEqual([
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ]);
+});
+
+test('Корректно работает, если в пропсах передать значение, которого нет в данном объекте', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+
+  expect(orderByProps(obj, ['name', 'class'])).toEqual([
+    { key: 'name', value: 'мечник' },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+  ]);
+});
+
+test('Не выдает ошибку, если не передать второй параметр', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+
+  expect(() => orderByProps(obj)).not.toThrow();
+});
